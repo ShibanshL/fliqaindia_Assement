2edd230f7d551286a4552cc2898f1fd4d5c455af
ShibanshL <shibansh.w@gmail.com>
2021-07-19 10:21:23 +0530

    This is  a test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Main from './New-component/Main'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Apirequest3 from './New-component/Apirequest3';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
       <Switch>
               <Route exact path="/" component={Main}/>
               <Route exact path="/API/:final" component={Apirequest3}/>
       </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
